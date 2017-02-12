1fd2a22b33c25c3de11e7d1cee65c9bad513cc3f
Juangui Restrepo <juangui.rp@gmail.com>
2017-02-12 16:15:06 -0500

    Se termina la actividad extra sobre material design

---
+++ 1_Interfaz/4_Extra_Material_Design/ActividadExtra/src/main/java/com/example/juangui/a4_extra_material_design/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.juangui.a4_extra_material_design;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
